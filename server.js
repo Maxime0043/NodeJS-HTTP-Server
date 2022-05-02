64e9ab04fd08d23b13ae813da4cf0876ce85fbcd
Pinna Maxime <max.pinna@hotmail.fr>
2022-05-02 15:44:36 +0200

    OK: Exécution du code uniquement sur la route '/' avec la méthode 'GET'

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("HELLO WORLD MAXIME");
+  // ROUTES
+  if (req.url === "/") {
+    // METHODE GET
+    if (req.method === "GET") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write("<h1>HELLO WORLD MAXIME</h1>");
+    }
+  }
+
   res.end();
 });
 
